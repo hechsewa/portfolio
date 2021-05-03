6a569da8706f773a8e7225dbd5d0fe39ca1737fe
Ewa Hechsman <ewa.hechsman@o2.pl>
2021-05-03 20:39:21 +0200

    mobile navigation

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,34 +1,66 @@
 import * as React from 'react'
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import './nav.css'
 import modedark from '../../images/mode-dark.svg'
 import modelight from '../../images/mode-light.svg'
 import logolight from '../../images/logo-light.svg'
 import logodark from '../../images/logo-dark.svg'
+import menu_dark from '../../images/menu-dark.svg'
+import menu_light from '../../images/menu_light.svg'
+import close_dark from '../../images/close_dark.svg'
+import close_light from '../../images/close_light.svg'
 
 const Navbar = (props) => {
 
+  const [navOut, setNavOut] = useState(false);
+
   useEffect(()=> {
-    console.log(window.location.pathname);
+    rollOutNav(); //check if mobile nav out
     var loc = window.location.pathname;
     var navitems = document.querySelectorAll(".nav-item.active");
+    var navitemsmobile = document.querySelectorAll(".nav-item-mobile.active");
     navitems.forEach(it => {
       it.className="nav-item";
+    });
+    navitemsmobile.forEach(itmobile => {
+      itmobile.className="nav-item-mobile";
     });
     switch (loc) {
       case '/':
         document.getElementById('projects').className += " active";
+        document.getElementById('projects-mobile').className += " active";
         return;
       case '/visuals':
         document.getElementById('visuals').className += " active";
+        document.getElementById('visuals-mobile').className += " active";
         return;
       case '/about':
         document.getElementById('about').className += " active";
+        document.getElementById('about-mobile').className += " active";
         return;
       default:
         return;
     }
   });
+
+  function rollOutNav() {
+    if (navOut) {
+      var nav_mobile = document.querySelectorAll('.nav-mobile.invisible')[0];
+      nav_mobile.className = 'nav-mobile';
+    } else {
+      var nav_mobile_out = document.querySelectorAll('.nav-mobile')[0];
+      nav_mobile_out.className='nav-mobile invisible';
+    }
+  }
+
+  function changeNav(e) {
+    if(e.target.id==='menuic') {
+      setNavOut(true);
+    } else {
+      setNavOut(false);
+    }
+    console.log(navOut);
+  }
 
   return (
     <>
@@ -41,7 +73,20 @@ const Navbar = (props) => {
             <li id='about' className='nav-item'><a className={props.theme==='dark'? '' : 'darkmode'}href="/about">About</a></li>
             <li className='nav-item nav-btn'><button type='button' onClick={props.setTheme}><img alt="change contrast icon" src={props.theme === 'dark' ? modelight : modedark}/></button></li>
           </div>
+          <div className='nav-icon'>
+            <img id='menuic' className={navOut ? 'invisible' : ''} src={props.theme==='dark'? menu_dark : menu_light} alt='menu icon' onClick={changeNav} />
+          </div>
         </ul>
+        <div className='nav-mobile invisible'>
+          <div className='nav-icon-mobile'>
+            <img id='closeic' src={props.theme==='dark'? close_dark : close_light} alt='close icon' onClick={changeNav} />
+          </div>
+          <div className='nav-links-mobile'>
+            <a id='projects-mobile' className='nav-item-mobile active' href="/">Projects</a>
+            <a id='visuals-mobile' className='nav-item-mobile' href="/visuals">Visuals</a>
+            <a id='about-mobile' className='nav-item-mobile' href="/about">About</a>
+          </div>
+        </div>
       </nav>
     </>
   )
