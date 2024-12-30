bd71943568c95a2c6861b7728b0682389d74bab8
hechsewa <ewahechsman@gmail.com>
2024-12-30 10:00:47 +0100

    window undef fix

--- src/components/Navbar/NavItem/navitem.js
+++ src/components/Navbar/NavItem/navitem.js
@@ -2,7 +2,9 @@ import React from "react";
 import './navitem.css';
 
 const Navitem = (props) => {
-    var currentPage = window.location.pathname;
+    if(typeof window !== 'undefined'){
+      var currentPage = window.location.pathname;
+    }
     console.log(currentPage);
     return (
         <div className={currentPage===props.href?'active':''}>
